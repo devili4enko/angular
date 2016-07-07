8cd8d75810ff5e5f69861b2eb35b571ac22da486
oleg.dekhtiarenko <oleg.dekhtiarenko@wonga.com>
2016-07-07 15:44:42 +0300

    services

--- AngularTest/Scripts/NgScripts/Controllers.js
+++ AngularTest/Scripts/NgScripts/Controllers.js
@@ -1,28 +1,53 @@
 var app = angular.module("app", []);
 
 
-var MainController = function ($scope, $http) {
+var MainController = function ($scope, gitHub, $interval,
+                               $log, $location, $anchorScroll) {
+
+    var countdownInterval = null;
 
     $scope.message = "Github Viewer";
     $scope.username = "angular";
-
-
-    $scope.search = function (username) {
-        console.log(username);
-        $http.get("https://api.github.com/users/" + username).then(onUserComplete);
-    }
-
-    var onRepos = function (response) {
-        $scope.repos = response.data;
-    };
-
-    var onUserComplete = function (response) {
-        $scope.gitUser = response.data;
-        $http.get($scope.gitUser.repos_url).then(onRepos);
-    };
-
     $scope.orderBy = "stargazers_count";
     $scope.orderType = "+";
+    var countdown = 5;
+
+    var startCountdown = function () {
+        countdownInterval = $interval(decrementCountdown, 1000, countdown);
+    };
+
+    $scope.search = function (username) {
+        $log.info("Seacring for " + username);
+        var t = gitHub.getUser(username);
+        console.log("search: "+ t);
+            t.then(onUserComplete);
+        if (countdownInterval) {
+            $interval.cancel(countdownInterval);
+        }
+    }
+
+    var onRepos = function (data) {
+        $scope.repos = data;
+
+        $location.hash('userDetails');
+        $anchorScroll();
+    };
+
+    var onUserComplete = function (data) {
+        $scope.gitUser = data;
+        var q = gitHub.getRepos($scope.gitUser);
+        console.log("onUserComplete: " + q);
+        q.then(onRepos);
+    };
+
+    var decrementCountdown = function () {
+        countdown -= 1;
+        if (countdown == 0) {
+            $scope.search($scope.username);
+        }
+    };
+
+    startCountdown();
 };
 
-app.controller("MainController", ["$scope", "$http", MainController]);
+app.controller("MainController", ["$scope", "gitHub", "$interval", "$log", "$location", "$anchorScroll", MainController]);
